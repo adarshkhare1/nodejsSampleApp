5db1f6d7c202a00007b5d98bf636bdc688872e3c
Adarsh Khare <adkhare@ebay.com>
2016-12-27 17:43:05 -0800

    Added a sample navigation menu.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var headerMaunItems = { 'Home': '/home', 'Contact': '/contact', 'About': '/about' };
+  res.render('index', { title: 'Agile Dashboard' 
+                    , headerMaunItems: headerMaunItems});
 });
 
 module.exports = router;
